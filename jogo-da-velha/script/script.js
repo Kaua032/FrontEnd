f9bc2dcec11cc9234c7f65f4e2eabe162c6425f6
Kauã Xavier <kauaxavier032@gmail.com>
2023-03-06 12:38:16 -0300

    lines in process

--- jogo-da-velha/script/script.js
+++ jogo-da-velha/script/script.js
@@ -6,37 +6,61 @@ let ocupation = [0,0,0,0,0,0,0,0,0]
 function analize(){
     if(i > 0){
         if(ocupation[0] == ocupation[1] && ocupation[0] == ocupation[2] && ocupation[0] == 1){
+            document.getElementById("0").innerHTML = "<img class='maior' src='./img-tracos/esquerdax.png' />"
+            document.getElementById("1").innerHTML = "<img class='maior' src='./img-tracos/meiox.png' />"
+            document.getElementById("2").innerHTML = "<img class='maior' src='./img-tracos/direitax.png' />"
             console.log("O x ganhou")
             return
         }
         else if(ocupation[0] == ocupation[1] && ocupation[0] == ocupation[2] && ocupation[0] == 2){
+            document.getElementById("0").innerHTML = "<img class='maior' src='./img-tracos/esquerdao.png' />"
+            document.getElementById("1").innerHTML = "<img class='maior' src='./img-tracos/meioesquerdapradireita.png' />"
+            document.getElementById("2").innerHTML = "<img class='maior' src='./img-tracos/direitao.png' />"
             console.log("O o ganhou") 
             return
         }
 
         else if(ocupation[2] == ocupation[5] && ocupation[2] == ocupation[8] && ocupation[2] == 1){
+            document.getElementById("2").innerHTML = "<img class='maior' src='./img-tracos/cimax.png' />"
+            document.getElementById("5").innerHTML = "<img class='maior' src='./img-tracos/meiodecimaabaixox.png' />"
+            document.getElementById("8").innerHTML = "<img class='maior' src='./img-tracos/baixox.png' />"
                 console.log("O x ganhou")
                 return
         }
         else if(ocupation[2] == ocupation[5] && ocupation[2] == ocupation[8] && ocupation[2] == 2){
+            document.getElementById("2").innerHTML = "<img class='maior' src='./img-tracos/cimao.png' />"
+            document.getElementById("5").innerHTML = "<img class='maior' src='./img-tracos/meiocimaparabaixo.png' />"
+            document.getElementById("8").innerHTML = "<img class='maior' src='./img-tracos/baixao.png' />"
             console.log("O o ganhou")
             return
         }
 
         else if(ocupation[8] == ocupation[7] && ocupation[8] == ocupation[6] && ocupation[8] == 1){
+            document.getElementById("8").innerHTML = "<img class='maior' src='./img-tracos/direitax.png' />"
+            document.getElementById("7").innerHTML = "<img class='maior' src='./img-tracos/meiox.png' />"
+            document.getElementById("6").innerHTML = "<img class='maior' src='./img-tracos/esquerdax.png' />"
                 console.log("O x ganhou")
                 return
         }
         else if(ocupation[8] == ocupation[7] && ocupation[8] == ocupation[6] && ocupation[8] == 2){
+            document.getElementById("8").innerHTML = "<img class='maior' src='./img-tracos/direitao.png' />"
+            document.getElementById("7").innerHTML = "<img class='maior' src='./img-tracos/meioesquerdapradireita.png' />"
+            document.getElementById("6").innerHTML = "<img class='maior' src='./img-tracos/esquerdao.png' />"
             console.log("O o ganhou")
                 return
         }
 
         else if(ocupation[0] == ocupation[3] && ocupation[0] == ocupation[6] && ocupation[0] == 1){
+            document.getElementById("0").innerHTML = "<img class='maior' src='./img-tracos/cimax.png' />"
+            document.getElementById("3").innerHTML = "<img class='maior' src='./img-tracos/meiodecimaabaixox.png' />"
+            document.getElementById("6").innerHTML = "<img class='maior' src='./img-tracos/baixox.png' />"
                 console.log("O x ganhou")
                 return
         }
         else if(ocupation[0] == ocupation[3] && ocupation[0] == ocupation[6] && ocupation[0] == 2){
+            document.getElementById("0").innerHTML = "<img class='maior' src='./img-tracos/cimao.png' />"
+            document.getElementById("3").innerHTML = "<img class='maior' src='./img-tracos/meiocimaparabaixo.png' />"
+            document.getElementById("6").innerHTML = "<img class='maior' src='./img-tracos/baixao.png' />"
             console.log("O o ganhou")
                 return
         }
